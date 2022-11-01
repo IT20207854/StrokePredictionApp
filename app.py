86701a131c53ed09884ecf06a1a4a06f60a86adb
IT20207854 <maleeshadesilva21@gmail.com>
2022-11-01 21:13:39 +0530

    updated app.py

--- app.py
+++ app.py
@@ -25,6 +25,8 @@ import base64
 # Creating the separation
 col1, col2 = st.columns([2, 1])
 
+# Adding background image
+
 
 def add_bg_from_local(image_file):
     with open(image_file, "rb") as image_file:
@@ -51,98 +53,52 @@ def display():
 
 def load_PreTrainedModelDetails():
     PreTrainedModelDetails = joblib.load(
-        'model/stroke_pred_classification.joblib')
+        "model/stroke_pred_classification.joblib")
     return PreTrainedModelDetails
 
 
-def prediction(input_df):
-
-    PreTrainedModelDetails = load_PreTrainedModelDetails()
-
-    # Random Forest Classifier
-    RandomForestClassifier = PreTrainedModelDetails.get('model')
-
-    # PreFitted Scaler
-    PreFittedScaler = PreTrainedModelDetails.get('scaler')
-
-    numerical_cols = PreTrainedModelDetails.get('numerical_cols')
-
-    categorical_cols = PreTrainedModelDetails.get('categorical_cols')
-
-    encoded_cols = PreTrainedModelDetails.get('encoded_cols')
-
-    input_df[categorical_cols] = PreFittedEncoder.transform(
-        input_df[categorical_cols])
-    input_df[numerical_cols] = PreFittedScaler.transform(
-        input_df[numerical_cols])
-
-    inputs_for_prediction = input_df[numerical_cols + encoded_cols]
-
-    prediction = RandomForestClassifier.predict(inputs_for_prediction)
-
-    if prediction == 0:
-        st.success("Patient is not at risk of getting a stroke.")
-    else:
-        st.warning("Patient is at risk of getting a stroke.")
-
-    # st.write("Accuracy of the prediction : {}".format(accuracy))
+def data_preprocessor(df):
+    df.gender = df.gender.map({'Male': 0, 'Female': 1})
+    df.hypertension = df.hypertension.map({'Yes': 1, 'No': 0})
+    df.heart_disease = df.heart_disease.map({'Yes': 1, 'No': 0})
+    df.ever_married = df.ever_married.map({'Married': 1, 'Unmarried': 0})
+    df.work_type = df.work_type.map(
+        {'Private Sector': 2, 'Government Sector': 0, 'Never Worked': 1, 'Self-employed': 3, 'Children': 4})
+    df.Residence_type = df.Residence_type.map({'Urban': 1, 'Rural': 0})
+    df.smoking_status = df.smoking_status.map(
+        {'Never Smoked': 2, 'Formerly Smoked': 1, 'Smokes': 3, 'Unknown': 0})
+    return df
 
 
 def get_user_input():
     form = col1.form(key='user input form')
 
-    gender_select = form.radio("gender", ['Male', 'Female', 'Other'], key='gender')
-    gender_vals = {'Female': 0,
-    'Male': 1,
-    'Other': 2}
-    gender = gender_vals[gender_select]
-    
+    gender = form.radio("gender", ['Male', 'Female', 'Other'], key='gender')
 
     age = form.number_input("age", 1, 120, key='age')
 
-    hypertension_select = form.radio(
+    hypertension = form.radio(
         "hypertension", ['Yes', 'No'], key='hypertension')
-    hypertension_vals = {'No': 0,
-    'Yes': 1}
-    hypertension = hypertension_vals[hypertension_select]
 
-    heart_disease_select = form.radio(
+    heart_disease = form.radio(
         "heart_disease", ['Yes', 'No'], key='heart_disease')
-    heart_disease_vals = {'No': 0,
-    'Yes': 1}
-    heart_disease = heart_disease_vals[heart_disease_select]
 
-    ever_married_select = form.radio(
+    ever_married = form.radio(
         "ever_married", ['Married', 'Unmarried'], key='ever_married')
-    ever_married_vals = {'No': 0,
-    'Yes': 1}
-    ever_married = ever_married_vals[ever_married_select]
 
-    work_type_select = form.radio("work_type", ['Private Sector', 'Government Sector', 'Never Worked',
-                                         'Self-employed', 'Children'], key='work_type')
-    work_type_vals = {'Private': 0,
-    'Self-employed': 1,
-    }
-    work_type = work_type_vals[work_type_select]
-    
-    Residence_type_select = form.radio(
+    work_type = form.radio("work_type", [
+                           'Private Sector', 'Government Sector', 'Never Worked', 'Self-employed', 'Children'], key='work_type')
+
+    Residence_type = form.radio(
         "Residence_type", ['Urban', 'Rural'], key='Residence_type')
-    Residence_type_vals = {'Urban': 1,
-    'Rural': 0,
-    }
-    Residence_type = Residence_type_vals[Residence_type_select]
 
     avg_glucose_level = form.number_input(
         "avg_glucose_level", 40.0, 400.0, key='avg_glucose_level')
-    
+
     bmi = form.number_input("bmi", 10.00, 120.00, key='bmi')
-    
-    smoking_status_select = form.radio("smoking_status", [
-        'Never Smoked', 'Formerly Smoked', 'Smokes', 'Unknown'], key='smoking_status')
-    smoking_status_vals = {'Urban': 1,
-    'Rural': 0,
-    }
-    Residence_type = Residence_type_vals[Residence_type_select]
+
+    smoking_status = form.radio("smoking_status", [
+                                'Never Smoked', 'Formerly Smoked', 'Smokes', 'Unknown'], key='smoking_status')
 
     submitButton = form.form_submit_button(label='Predict Stroke Condition')
 
@@ -162,18 +118,27 @@ def get_user_input():
 
         }
 
-        user_input = pd.DataFrame([SingleUserInput])
+        user_input = pd.DataFrame(SingleUserInput, index=[0])
 
         return user_input
 
 
-def main():
-    display()
-    input_details_df = get_user_input()
+user_input_df = get_user_input()
+processed_user_input = data_preprocessor(user_input_df)
 
-    if input_details_df is not None:
-        prediction(input_details_df)
+PreTrainedModelDetails = load_PreTrainedModelDetails()
 
+# Random Forest Classifier
+DecisionTreeClassifier = PreTrainedModelDetails.get('model')
+
+prediction = DecisionTreeClassifier.predict(processed_user_input)
+
+st.subheader('Prediction')
+
+if prediction == 0:
+    st.success("Patient is not at risk of getting a stroke.")
+else:
+    st.warning("Patient is at risk of getting a stroke.")
 
 # Sidebar Configurations
 st.sidebar.header("Introduction")
@@ -182,10 +147,6 @@ with st.sidebar:
     st.write("A stroke, sometimes called a brain attack, occurs when something blocks blood supply to part of the brain or when a blood vessel in the brain bursts. In either case, parts of the brain become damaged or die. A stroke can cause lasting brain damage, long-term disability, or even death.")
 
 st.sidebar.image("images/stroke9.jfif", use_column_width=True)
-
-
-if __name__ == '__main__':
-    main()
 
 # Image grid
 col2.image("images/stroke1.jpg")
